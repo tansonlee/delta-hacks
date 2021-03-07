667de866019cb6bdd0f485a3a6cd7dbf5bb09ae7
tansonlee <leetanson@gmail.com>
2021-03-06 20:07:33 -0500

    added route to vaccine tracker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import VaccinationsPage from "./Pages/VaccinationsPage/VaccinationsPage";
+import VaccineTrackerPage from "./Pages/VaccineTrackerPage/VaccineTrackerPage";
 import CasesPage from "./Pages/CasesPage/CasesPage";
 import Navbar from "./Navbar/Navbar";
 
@@ -14,6 +15,10 @@ function App() {
 				<Switch>
 					<Route path="/" exact component={HomePage} />
 					<Route path="/vaccinations" component={VaccinationsPage} />
+					<Route
+						path="/vaccine-tracker"
+						component={VaccineTrackerPage}
+					/>
 					<Route path="/cases" component={CasesPage} />
 				</Switch>
 			</Router>
