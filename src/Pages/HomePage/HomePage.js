0034294a2d9e75d2f494d738e2f2ffc25107eba7
danielle-fong <daniellefong13@gmail.com>
2021-03-06 22:27:54 -0500

    home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -6,17 +6,17 @@ function HomePage() {
 		<>
 			<div className="intro-block">
 				<h1 className="title">COVID-19</h1>
-				<h1 className="title">vaccine tracker</h1>
+				<h1 className="heading">VACCINE TRACKER</h1>
 				<h1 className="subtitle">Educate yourself on how to protect yourself from the virus.</h1>
 			</div>
-			<hr />
+
 			<div className="about-block">
 				<h2 className="about">Why Vacc Tracc</h2>
 				<h2 className="about-text">hdeiufhfhehufhiuhei</h2>
 			</div>
-			<hr />
+
 			<div className="learn-block">
-				<h3 className="learn-more">LEARN MORE:</h3>
+				<h3 className="learnmore">LEARN MORE:</h3>
 				
 				<div className="link">
 					<a href="https://www.ontario.ca/page/government/"> Government of Ontario </a>
