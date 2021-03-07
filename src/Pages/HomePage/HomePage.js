9e0b6b469d809ace4af8217f29e85c0e487db76d
danielle-fong <daniellefong13@gmail.com>
2021-03-07 02:27:08 -0500

    home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,14 +5,29 @@ function HomePage() {
 	return(
 		<>
 			<div className="intro-block">
+				
 				<h1 className="title">COVID-19</h1>
 				<h1 className="heading">VACCINE TRACKER</h1>
 				<h1 className="subtitle">Educate yourself on how to protect yourself from the virus.</h1>
 			</div>
 
 			<div className="about-block">
-				<h2 className="about">Why Vacc Tracc</h2>
-				<h2 className="about-text">hdeiufhfhehufhiuhei</h2>
+				<h2 className="about">Why VaccTracc</h2>
+				<h2 className="about-text">
+					<h2 className="text1">
+					COVID-19 has had a huge impact on our lives. With Ontario beginning to pick up on their 
+					vaccination timeline, we recognized that there is currently no data to track the number of 
+					vaccinations per region - only total number of vaccinations across the province. 
+					This data, in conjunction with the current provincial framework, could be critically used 
+					by our government to troubleshoot potential logistics issues, provide information on localized 
+					resistance to vaccinations, and concurrently ensure that current frameworks and policies are effective.
+					</h2>
+					<h2 className="text2">
+					As such, we propose VaccTracc, a tool that provides clear information to the public on the inoculation 
+					progress of vaccinations within their district whilst providing data that supports our government’s 
+					framework to COVID vaccine rollouts.
+					</h2>
+				</h2>
 			</div>
 
 			<div className="learn-block">
