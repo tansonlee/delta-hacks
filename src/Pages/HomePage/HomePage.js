65f0d0040b2a65fa12fdf7c1bfbc7132f2574e43
danielle-fong <daniellefong13@gmail.com>
2021-03-06 19:32:28 -0500

    home changes

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function HomePage() {
-	return <div>this is our home page</div>;
+	return 
+	<div className="intro-block">
+		<h1 className="title">COVID-19</h1>
+		<h1 className="title">vaccine tracker</h1>
+		<h1 className="subtitle">Educate yourself on how to protect yourself from the virus.</h1>
+	</div>
+
+
+	;
 }
 
 export default HomePage;
