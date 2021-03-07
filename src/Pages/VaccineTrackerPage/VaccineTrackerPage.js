9e7046b9810143fb54755f0f872ecae22c8b4bb1
chanp2002 <paytonstuff1@gmail.com>
2021-03-06 20:04:17 -0500

    add da map

--- src/Pages/VaccineTrackerPage/VaccineTrackerPage.js
+++ src/Pages/VaccineTrackerPage/VaccineTrackerPage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function VaccineTrackerPage() {
-	return <div>This is where we put da map page</div>;
+	const position = [51.505, -0.09]
+
+	return <div>
+		<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>,
+	</div>;
 }
 
 export default VaccineTrackerPage;
